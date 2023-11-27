54697fbdcfbc810dd2eed3050afc5b75a707a9bf
Anant kumar <anant1042005@gmail.com>
2023-11-28 00:04:48 +0530

    changes made

---
+++ script.js
@@ -0,0 +1,9 @@
+const studentLogin = document.getElementById("student-btn");
+const studentCrossBtn = document.getElementById("studentCrossBtn");
+
+function studentBtn(){
+    const StudentContainer = document.getElementById("studentContainer").style.opacity = "1";
+}
+function studentCancel(){
+    const StudentContainer = document.getElementById("studentContainer").style.opacity = "0";
+}
