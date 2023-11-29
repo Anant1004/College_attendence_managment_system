8b69b760d98daa0b0da3c5e21eba0df49ac5bc75
Akash <akashbca9508@gmail.com>
2023-11-29 23:24:42 +0530

    added new theme

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const studentLogin = document.getElementById("student-btn");
-const teacherLogin = document.getElementById("teacher-btn");
-const studentCrossBtn = document.getElementById("studentCrossBtn");
+const popstudent = document.getElementById("popstudent");
+const popteacher = document.getElementById("popteacher");
+const teacherclose = document.getElementById("teacherclose")
+;const studentclose = document.getElementById("studentclose");
 
-function studentBtn(){
-    const StudentContainer = document.getElementById("studentContainer").style.transform = "scale(1)";
+function teacherbtn(){
+  popteacher.style.transform = "scale(1)";
 }
-function studentCancel(){
-    const StudentContainer = document.getElementById("studentContainer").style.transform = "scale(0)";
+function studentbtn(){
+   popstudent.style.transform = "scale(1)";
 }
-function teacherBtn(){
-    const teacherContainer = document.getElementById("teacherContainer").style.transform = "scale(1)";
+function teacherclosebtn(){
+  popteacher.style.transform = "scale(0)";
 }
-function teacherCancel(){
-    const teacherContainer = document.getElementById("teacherContainer").style.transform = "scale(0)";
+function studentclosebtn(){
+  popstudent.style.transform = "scale(0)";
 }
+
+
