b164c8a4b2fe864291e8a188436e3a1d8d257b48
Akash <akashbca9508@gmail.com>
2023-12-18 00:37:40 +0530

    teacher dashboard updated

--- scripts/teacher_Dashboard.js
+++ scripts/teacher_Dashboard.js
@@ -1,16 +1,13 @@
-function onClickMenu(){
+function onClickMenu() {
     document.getElementById("menu").classList.toggle("icon");
     document.getElementById("sidepanel").classList.toggle("change")
 
 }
 
 const navcontainer = document.getElementById("nav-container");
-const navOptions = document.querySelectorAll(".nav-option"); // assuming navOption is a class
-const navSpans = document.querySelectorAll(".nav-span"); // assuming navSpan is a class
 const menubtn = document.getElementById("navigation");
-const mainteachercontainer = document.getElementById("mainTeacherContainer");
-
-menubtn.onclick = function() {
+const mainSection = document.querySelector("section");
+menubtn.onclick = function () {
     navcontainer.classList.toggle("expand-navbar");
-    mainteachercontainer.classList.toggle("expand-main");
+    mainSection.classList.toggle("expand-main");
 }
